2c0353923aad005377c9cb766f23ec2d1a0d2304
Raghav <raghavawasthi240@gmail.com>
2025-04-19 08:09:14 +0530

    chore: output dir changed (dist-> build)

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   build: {
+    outDir: 'build', // <- This changes the output directory from dist to build
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
